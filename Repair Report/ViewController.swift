540abda1d5c92a1d8eec506d432d6b2528409405
Nientai Ho <hntddt123@gmail.com>
2018-06-14 19:41:57 -0500

    Initial Commit

---
+++ Repair Report/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Repair Report
+//
+//  Created by Nientai Ho on 2018/6/14.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
