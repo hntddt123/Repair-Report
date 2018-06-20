9979359fb201a75fef66bcf4d87da96071e08310
Nientai Ho <hntddt123@gmail.com>
2018-06-20 16:25:47 -0500

    Working on add new form view controller

---
+++ Repair Report/NewFormViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewFormViewController.swift
+//  Repair Report
+//
+//  Created by Nientai Ho on 2018/6/20.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+import UIKit
+
+class NewFormViewController: UIViewController {
+    
+    @IBOutlet weak var applicant: UITextField!
+    @IBOutlet weak var fillDate: UITextField!
+    @IBOutlet weak var equipmentName: UITextField!
+    @IBOutlet weak var equipmentSerialNumber: UITextField!
+    @IBOutlet weak var propertyNumber: UITextField!
+    @IBOutlet weak var eventDescription: UITextField!
+    
+    
+}
