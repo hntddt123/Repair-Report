0867e37418d7329a25fb5c4b94d1c214a189d50c
Nientai Ho <hntddt123@gmail.com>
2018-07-03 20:21:03 -0500

    Complete CoreData Saving part

--- Repair Report/WorkCellTableViewController.swift
+++ Repair Report/WorkCellTableViewController.swift
@@ -7,16 +7,16 @@
 //
 
 import UIKit
-
+import CoreData
 
 class WorkCellTableViewController: UITableViewController {
    
-    var cellArrayName = ["Report 1","Report 2","Report 3"]
-    var cellDetailArrayName = ["2018/06/12","2018/06/13","2018/06/14"]
+    //Cellarray contains "Report x"
+    //Cellarray date contains "2018/06/12 for right detail"
+    var reports: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,38 +30,67 @@ class WorkCellTableViewController: UITableViewController {
     @IBAction func addNewForm(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "New Report", message: "Enter report name", preferredStyle: .alert)
         
+        //Textbox decoration
         alert.addTextField(configurationHandler: { (textField) in
             textField.placeholder = "Name"
         })
+        //Cancel Action
         alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: " Default action"), style: .cancel , handler: { _ in
-            //Cancel Action
         }))
+        //OK Action
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            
             //Date format for new entry
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy/MM/dd"
             let date = Date()
             let dateString = dateFormatter.string(from: date)
-            //Append data to array
-            //cellArrayName.append("Report")
-            let name = (alert.textFields?.first?.text)!
-            self.cellArrayName.append(name)
-            self.cellDetailArrayName.append("\(dateString)")
+            //Append data to NSManagedObject array
+            let titleToSave = (alert.textFields?.first?.text)!
+            self.save(with: titleToSave, with: dateString)
+            
+            //Update Table
             self.tableView.beginUpdates()
-            self.tableView.insertRows(at: [IndexPath(row: self.cellArrayName.count-1, section: 0)], with: .automatic)
+            self.tableView.insertRows(at: [IndexPath(row: self.reports.count-1, section: 0)], with: .automatic)
             self.tableView.endUpdates()
         }))
         self.present(alert, animated: true, completion: nil)
     }
     
+    func save(with title: String, with date: String) {
+
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else {
+                return
+        }
+
+        //NSObject context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        //Insert to NSManagedObject
+        let entity = NSEntityDescription.entity(forEntityName: "Report",
+                                                in: managedContext)!
+        let report = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        //key-value coding
+        report.setValue(title, forKeyPath: "reportTitle")
+        report.setValue(date, forKeyPath: "fillDate")
+
+        //save
+        do {
+            try managedContext.save()
+            reports.append(report)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        //return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return cellArrayName.count
+        return reports.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -70,12 +99,22 @@ class WorkCellTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reportCell", for: indexPath)
-        cell.textLabel?.text = cellArrayName[indexPath.row]
-        cell.detailTextLabel?.text = cellDetailArrayName[indexPath.row]
+        let report = reports[indexPath.row]
+        cell.textLabel?.text = report.value(forKeyPath: "reportTitle") as? String
+        cell.detailTextLabel?.text = report.value(forKeyPath: "fillDate") as? String
 
         return cell
     }
     
+//    private func updateDatabase(with report: [String]) {
+//        container?.performBackgroundTask({ (context) in
+//            for name in self.cellArrayName {
+//                //add name
+//                let request = NSFetchRequest<NSManagedObject>(entityName: "title")
+//
+//            }
+//        })
+//    }
     
     
 
