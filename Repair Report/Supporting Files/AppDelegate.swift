516747dc7eab1eeb010730096cbd6d4c49c714ca
Nientai Ho <hntddt123@gmail.com>
2018-07-10 18:13:00 -0500

    Added Navigation bar Color

--- Repair Report/Supporting Files/AppDelegate.swift
+++ Repair Report/Supporting Files/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Set navigation bar color
+        UINavigationBar.appearance().barTintColor = UIColor.orange
+        UINavigationBar.appearance().barStyle = UIBarStyle.black
+        UINavigationBar.appearance().tintColor = .white
+        
         return true
     }
 
